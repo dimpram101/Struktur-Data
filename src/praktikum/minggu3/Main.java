8edbf62a3d6d599c0170d0ab5f8bbd2797451c97
dimpram101 <64238889+dimpram101@users.noreply.github.com>
2022-09-28 23:01:33 +0800

    test

--- src/praktikum/minggu3/Main.java
+++ src/praktikum/minggu3/Main.java
@@ -1,5 +1,17 @@
 package praktikum.minggu3;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    LinkedList linkedList = new LinkedList();
+
+    linkedList.add(8);
+    linkedList.add(1);
+    linkedList.add(3);
+    linkedList.add(4);
+    linkedList.add(9);
+
+    System.out.println(linkedList);
+    System.out.println(linkedList.findNode(5).getValue());
+
+  }
 }
